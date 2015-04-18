fe8c1a3dfd6df1fa5faf49006c1f06b53d067792
Éverton Ribeiro <nuxlli@gmail.com>
2015-04-18 04:44:14 -0300

    [travis] Adding start-stop-daemon to run azk agent

--- src/libexec/script_ci.sh
+++ src/libexec/script_ci.sh
@@ -3,6 +3,6 @@
 sudo mkdir /etc/resolver/
 echo "nameserver 192.168.50.4.53" | sudo tee /etc/resolver/dev.azk.io
 ./bin/azk nvm node -e "require('azk').meta.set('tracker_permission', false);"
-./bin/azk agent start
+/sbin/start-stop-daemon --start --pidfile /tmp/azk_server_99.pid --make-pidfile --background exec ./bin/azk agent start
 ./src/libexec/script_ci.sh
 ./bin/azk nvm npm run test:slow
